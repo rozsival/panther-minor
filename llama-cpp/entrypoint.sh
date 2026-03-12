810c25bd1719c9f2033100a12c35ea377c3648e8
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-12 15:26:42 +0100

    fix: remove models directory argument from llama-server entrypoint

--- llama-cpp/entrypoint.sh
+++ llama-cpp/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # -- llama.cpp server entrypoint ----------------------------------------------
-exec /opt/llama-cpp/build/bin/llama-server --host 0.0.0.0 --port 8000 --models-dir /models/.huggingface --models-preset /models/preset.ini
+exec /opt/llama-cpp/build/bin/llama-server --host 0.0.0.0 --port 8000 --models-preset /models/preset.ini
