74666a7ac4c2a0904f6829fce23ef7b62486fc7d
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-12 13:15:41 +0100

    fix: update entrypoint script and Makefile for model configuration

--- llama-cpp/entrypoint.sh
+++ llama-cpp/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # llama.cpp server entrypoint script to load parameters from model.json and launch the server
 MODEL=${1:-"panther-minor"}
-CONFIG_FILE="/models/$MODEL/model.json"
+CONFIG_FILE="/models/$MODEL.json"
 
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "Error: Model file \"$CONFIG_FILE\" not found."
@@ -10,10 +10,9 @@ fi
 
 # Find base parameters
 HF=$(jq -r '.model' "$CONFIG_FILE")
-SYSTEM_PROMPT="/models/$MODEL/system.txt"
 
 # Init ARGS with Hugging Face model and system prompt
-ARGS="--hf $HF --system-prompt-file $SYSTEM_PROMPT"
+ARGS="--hf $HF"
 
 # Dynamically add parameters from config.json
 while IFS=$'\t' read -r key value; do
@@ -27,4 +26,4 @@ while IFS=$'\t' read -r key value; do
 done < <(jq -r '.params | to_entries | .[] | "\(.key)\t\(.value)"' "$CONFIG_FILE")
 
 echo "Launching llama-server with dynamic args: $ARGS"
-exec /app/llama-server --host 0.0.0.0 --port 8000 --flash-attn on --numa distribute --split-mode layer "$ARGS"
+exec /app/llama-server --host 0.0.0.0 --port 8000 --flash-attn on --models-max 1 --numa distribute --split-mode layer "$ARGS"
