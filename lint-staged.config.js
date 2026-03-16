c991209332ff61bfae48eccea20b8bb66cfddc64
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-16 13:42:45 +0100

    style: standardize quotes in configuration and script files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 export default {
-  "*.{js,json,md,yml}": "prettier --write",
+  '*.{js,json,md,yml}': 'prettier --write',
 };
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  plugins: [import.meta.resolve('prettier-plugin-packagejson')],
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
