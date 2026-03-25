ff7735242d858861512af2db9ac2f19ceef79633
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-25 15:58:30 +0100

    feat(cli): add 'status' command to show cluster containers status

---
+++ bin/src/cluster_status_command.sh
@@ -0,0 +1,8 @@
+panther_cluster_status() {
+  cluster_status=$(panther_compose ps --format json)
+  formatted_status=$(echo "$cluster_status" | jq -r '. | "\(.Name)\t\(.State)\t\(.Status)"' | column -t -s $'\t')
+  panther_log_info 'Cluster status:'
+  echo "$formatted_status"
+}
+
+panther_cluster_start
