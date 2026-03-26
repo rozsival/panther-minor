b776664d5251761af89fa1faa3e01feb8cb7ea57
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-26 16:14:14 +0100

    fix(cli): improve service not found message in cluster validation

--- bin/src/lib/validations/validate_cluster_service.sh
+++ bin/src/lib/validations/validate_cluster_service.sh
@@ -4,12 +4,10 @@ validate_cluster_service() {
   cluster_services="$(panther_compose "${compose_args[@]}")"
   service_names="$(echo "$cluster_services" | jq -r '. | "\(.Service)"')"
 
-  if ! grep -q "$1" <<<"$service_names"; then
-    echo "Service '$1' not found in cluster"
-    echo "Available services:"
-    echo "$service_names" | awk '{print $1}' | tail -n +2
-    return 1
+  if ! echo "$service_names" | grep -q "^$1$"; then
+    echo "service '$1' not found in cluster"
+    echo "available services:"
+    echo "$service_names"
+    return
   fi
-
-  return 0
 }
