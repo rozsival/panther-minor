b883f7d8b10e697e8c62a934dba6b75c2ef083f3
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-26 16:45:36 +0100

    fix(cli): improve service validation logic in cluster commands

--- bin/src/lib/validations/validate_cluster_service.sh
+++ bin/src/lib/validations/validate_cluster_service.sh
@@ -1,5 +1,8 @@
 validate_cluster_service() {
-  if ! panther_services | grep -q "^$1$"; then
+  local service_names
+  service_names=$(panther_services)
+
+  if ! "$service_names" | grep -q "^$1$"; then
     printf "service '%s' not found in cluster\n\n" "$1"
     printf "available services:\n\n%s" "$service_names"
     return
