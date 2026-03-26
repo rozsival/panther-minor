0822b6317815544203b48ef029c3d084024f44ac
Vít Rozsíval <mail@vitrozsival.cz>
2026-03-26 15:02:37 +0100

    feat(cli): implement panther_logs function for improved log handling

--- bin/src/logs_command.sh
+++ bin/src/logs_command.sh
@@ -1,5 +1,6 @@
-echo "# This file is located at 'src/logs_command.sh'."
-echo "# It contains the implementation for the 'cli logs' command."
-echo "# The code you write here will be wrapped by a function named 'cli_logs_command()'."
-echo "# Feel free to edit this file; your changes will persist when regenerating."
-inspect_args
+panther_logs() {
+  # shellcheck disable=SC2154
+  panther_logs_service "${args[service]}"
+}
+
+panther_logs
