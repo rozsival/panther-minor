21fc656ae511a29c8d74a97ab3e94a45ecc05053
Vít Rozsíval <mail@vitrozsival.cz>
2026-04-16 20:52:37 +0200

    fix(cli): correct path for model remove cmd

--- bin/src/models_remove_command.sh
+++ bin/src/models_remove_command.sh
@@ -2,8 +2,15 @@ panther_models_remove() {
   local model="${args[model]}"
   panther_assert_supported_model "$model"
 
-  local target_file="$PANTHER_MODELS_DIR/.huggingface/$model.gguf"
-  if [[ ! -f "$target_file" ]]; then
+  local model_config model_name target_dir
+  model_config="$(panther_model_config "$model")"
+
+  [[ -n "$model_config" ]] || panther_log_error "Model '$model' not found in $(panther_models_config_file)"
+
+  model_name="$(jq -r '.name' <<<"$model_config")"
+  target_dir="$PANTHER_MODELS_DIR/.huggingface/$model_name"
+
+  if [[ -d "$target_dir" ]]; then
     panther_log_error "Model '$model' not found in local directory"
   fi
 
@@ -14,7 +21,7 @@ panther_models_remove() {
     exit 0
   fi
 
-  rm "$target_file"
+  rm -rf "$target_dir"
   panther_log_success "Model '$model' removed."
 }
 
